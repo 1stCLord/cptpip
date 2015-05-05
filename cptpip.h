b6070cf8db145215cafbae92d9f292e39063b551
Andrew Haining <thesombrerokid@ratusapparatus.com>
2015-05-06 00:09:41 +0100

    header was corrupt in initial commit

--- cptpip.h
+++ cptpip.h
@@ -1,3 +1,15 @@
+//
+//  C-PTPIP.h
+//
+//  Created by Andrew Haining on 22/04/2015.
+//
+//
+
+#ifndef __C_PTPIP__
+#define __C_PTPIP__
+
+#include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 enum Code
@@ -11,11 +23,13 @@ enum Code
   C_Mode = 0xD604
 };
 
-bool cptpipInit(const char* address, int *socket, int *eventSocket, uint32_t *s;
+bool cptpipInit(const char* address, int *socket, int *eventSocket, uint32_t *sessionId);
 void cptpipUninit(int socket, int eventSocket, uint32_t sessionId);
 
 uint32_t cptpipGetMode(int socket, uint32_t transactionId);
-void cptpipGetAllProperties(int socket, uint32_t transactionId, uint8_t **raw_p;
-void cptpipGetProperty(enum Code code, uint8_t const *raw_property_list, uint16;
+void cptpipGetAllProperties(int socket, uint32_t transactionId, uint8_t **raw_property_list, uint16_t *raw_property_list_length);
+void cptpipGetProperty(enum Code code, uint8_t const *raw_property_list, uint16_t raw_property_list_length, uint8_t const **values, uint8_t * value_size, uint8_t * value_count);
 
 #endif /* defined(__C_PTPIP__) */
+
+
